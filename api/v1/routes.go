8198af5a8f8980b3223e5f769e87ed1c332c74cd
nealyguo <nealyguo@tencent.com>
2023-08-15 11:12:56 +0800

    fix receiving 400 when user register

--- api/v1/routes.go
+++ api/v1/routes.go
@@ -6,9 +6,16 @@ import "github.com/gin-gonic/gin"
 func SetupRoutes(router *gin.RouterGroup) {
 	authGroup := router.Group("/user")
 	{
-		authGroup.POST("/register/", UserRegisterHandler)
+		authGroup.POST("/register/", UserRegisterHandler_origin)
 		authGroup.POST("/login", UserLoginHandler)
 
 		// 在这里添加对应的路由
+		authGroup.POST("/douyin/publish/action/", UserPublishHandler)
+	}
+
+	pubGroup := router.Group("/publish")
+	{
+		
+		pubGroup.POST("/action/", UserPublishHandler)
 	}
 }
