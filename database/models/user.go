50b76ef78419c2f2004a2479e98db3fccd844605
0g-m0 <2742955098@qq.com>
2023-08-19 15:31:24 +0800

    用户模块已完成

--- database/models/user.go
+++ database/models/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	ID              int64      `gorm:"column:id;primary_key"`
-	Username        string     `gorm:"column:name"`
+	Name            string     `gorm:"column:name"`
 	FollowCount     int64      `gorm:"column:follow_count"`
 	FollowerCount   int64      `gorm:"column:follower_count"`
 	IsFollow        bool       `gorm:"column:is_follow"`
@@ -17,8 +17,21 @@ type User struct {
 	WorkCount       int64      `gorm:"column:work_count"`
 	FavoriteCount   int64      `gorm:"column:favorite_count"`
 	Password        string     `gorm:"column:password"`
-	Token           string     `gorm:"column:token"`
 	CreatedAt       time.Time  `gorm:"column:created_at"`
 	UpdatedAt       time.Time  `gorm:"column:updated_at"`
 	DeletedAt       *time.Time `gorm:"column:deleted_at;index"`
+}
+
+type UserDTO struct {
+	ID              int64  `json:"id"`
+	Name            string `json:"name"`
+	FollowCount     int64  `json:"follow_count"`
+	FollowerCount   int64  `json:"follower_count"`
+	IsFollow        bool   `json:"is_follow"`
+	Avatar          string `json:"avatar"`
+	BackgroundImage string `json:"background_image"`
+	Signature       string `json:"signature"`
+	TotalFavorited  int64  `json:"total_favorited"`
+	WorkCount       int64  `json:"work_count"`
+	FavoriteCount   int64  `json:"favorite_count"`
 }
